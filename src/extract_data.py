ccd4bb037d3c53682528620057357acd9bbde70b
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-04-09 15:49:22 -0500

    creando la sesion de spark

--- src/extract_data.py
+++ src/extract_data.py
@@ -2,4 +2,6 @@ from pyspark.sql import SparkSession
 import pandas as pd
 import numpy as np
 import json,os,requests
-print('xd')
+
+spark_s=(SparkSession.builder.appName("Extract Data").getOrCreate())
+print(spark_s.context)
