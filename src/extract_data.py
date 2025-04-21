fffa539856a4ed12930ab98bd8af047280c32255
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-04-20 21:21:01 -0500

    ready for test

--- src/extract_data.py
+++ src/extract_data.py
@@ -6,14 +6,16 @@ import json,os,requests
 def fetch_data(offset,limit,url):
     params={'$offset':offset,'$limit':limit}
     r=requests.get(url=url,params=params)
-    return r.json
+    return r.json()
 
 spark_s=(SparkSession.builder.appName("Extract Data").getOrCreate())
 print(spark_s.sparkContext)
 data=fetch_data(offset=0,limit=10000,url='https://www.datos.gov.co/resource/s54a-sgyg.json')
-pdf=pd.DataFrame(data)
-pdf['fecha_iso'] = pd.to_datetime(pdf['fechaobservacion'])
-pdf.sort_values(by='fecha_iso', ascending=True, inplace=True)
-spark_df = spark_s.createDataFrame(pdf)
+print(type(data))
+spark_df=spark_s.createDataFrame(data)
+spark_df=spark_df.withColumn('fechaobservacion',spark_df['fechaobservacion'].cast("timestamp"))
+spark_df=spark_df.orderBy(spark_df['fechaobservacion'],ascending=True)
+spark_df.printSchema()
+spark_df.show(10)
 hdfs_output_path ="hdfs:///user/hadoop"
 spark_df.write.mode("overwrite").parquet(hdfs_output_path)
