40dae5a064c499f721edd437a340b5675b0d1b2c
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-04-30 11:30:30 -0500

    check

--- src/test_c.py
+++ src/test_c.py
@@ -2,43 +2,43 @@ from pyspark.sql import SparkSession
 import json,os,requests
 
 capitales_departamentos = [
-    "LETICIA",        # Amazonas
-    "MEDELLÍN",       # Antioquia
-    "ARAUCA",         # Arauca
-    "BARRANQUILLA",   # Atlántico
-    "CARTAGENA",      # Bolívar
-    "TUNJA",          # Boyacá
-    "MANIZALES",      # Caldas
-    "FLORENCIA",      # Caquetá
-    "YOPAL",          # Casanare
-    "POPAYÁN",        # Cauca
-    "VALLEDUPAR",     # Cesar
-    "QUIBDÓ",         # Chocó
-    "MONTERÍA",       # Córdoba
-    "BOGOTA, D.C",         # Cundinamarca (distrito capital)
-    "INÍRIDA",        # Guainía
-    "SAN JOSÉ DEL GUAVIARE", # Guaviare
-    "NEIVA",          # Huila
-    "RIOHACHA",       # La Guajira
-    "SANTA MARTA",    # Magdalena
-    "VILLAVICENCIO",  # Meta
-    "PASTO",          # Nariño
-    "CÚCUTA",         # Norte de Santander
-    "MOCOA",          # Putumayo
-    "ARMENIA",        # Quindío
-    "PEREIRA",        # Risaralda
-    "SAN ANDRÉS",     # San Andrés y Providencia
-    "BUCARAMANGA",    # Santander
-    "SINCELEJO",      # Sucre
-    "IBAGUÉ",         # Tolima
-    "CALI",           # Valle del Cauca
-    "MITÚ",           # Vaupés
-    "PUERTO CARREÑO"  # Vichada
+    'LETICIA',        # Amazonas
+    'MEDELLÍN',       # Antioquia
+    'ARAUCA',         # Arauca
+    'BARRANQUILLA',   # Atlántico
+    'CARTAGENA',      # Bolívar
+    'TUNJA',          # Boyacá
+    'MANIZALES',      # Caldas
+    'FLORENCIA',      # Caquetá
+    'YOPAL',          # Casanare
+    'POPAYÁN',        # Cauca
+    'VALLEDUPAR',     # Cesar
+    'QUIBDÓ',         # Chocó
+    'MONTERÍA',       # Córdoba
+    'BOGOTA, D.C',    # Cundinamarca (distrito capital)
+    'INÍRIDA',        # Guainía
+    'SAN JOSÉ DEL GUAVIARE', # Guaviare
+    'NEIVA',          # Huila
+    'RIOHACHA',       # La Guajira
+    'SANTA MARTA',    # Magdalena
+    'VILLAVICENCIO',  # Meta
+    'PASTO',          # Nariño
+    'CÚCUTA',         # Norte de Santander
+    'MOCOA',          # Putumayo
+    'ARMENIA',        # Quindío
+    'PEREIRA',        # Risaralda
+    'SAN ANDRÉS',     # San Andrés y Providencia
+    'BUCARAMANGA',    # Santander
+    'SINCELEJO',      # Sucre
+    'IBAGUÉ',         # Tolima
+    'CALI',           # Valle del Cauca
+    'MITÚ',           # Vaupés
+    'PUERTO CARREÑO'  # Vichada
 ]
 
 
 def fetch_data(offset,limit,url,city):
-      params={"$offset":offset,'$limit':limit,'$order':'fechaobservacion',"$where":f"municipio = '{city}'"}
+      params={"$offset":offset,"$limit":limit,"$order":'fechaobservacion',"$where":f"municipio = '{city}'"}
       r=requests.get(url=url,params=params)
       print(r.status_code)
       return r.json()
