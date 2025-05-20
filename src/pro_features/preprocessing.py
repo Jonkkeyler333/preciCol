f375efd912db4b0f498c15973049cb2a0418643c
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-05-20 13:55:39 -0500

    solve orderby

--- src/pro_features/preprocessing.py
+++ src/pro_features/preprocessing.py
@@ -8,14 +8,14 @@ import numpy as np
 def preprocess(path):
     df=spark.read.parquet(path)
     df.printSchema()
-    df=df.sortBy('fechaobservacion',ascending=True)
+    df=df.orderBy('fechaobservacion',ascending=True)
     null_counts=df.select([F.sum(c).isNull().cast('int').alias(c) for c in df.columns])
     print('Missing values',null_counts,sep='\n')
     df=df.drop([F.col('codigoestacion'),F.col('codigosensor'),F.col('departamento'),F.col('descripcionsensor'),F.col('nombreestacion'),F.col('unidadmedida')])
     df_t=df.groupby(F.window(F.col('fechaobservacion'),"10 minutes")
     ).agg(F.sum('valorobservado').alias()
     ).selectExpr("window.start as fecha_10min","precip_10min")
-    df_t=df_t.sortBy("fecha_10min")
+    df_t=df_t.orderBy("fecha_10min")
     df_t.show(5,False)
     
 
