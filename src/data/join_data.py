bb41c9270ce068c24066ca72aa399266df1b237d
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-06-01 18:08:55 -0500

    join data

--- src/data/join_data.py
+++ src/data/join_data.py
@@ -13,5 +13,23 @@ def city_column(df,cid):
     return df
 
 if __name__ == "__main__":
-    
+    CITIES=['SOLEDAD','CARTAGENA DE INDIAS','VALLEDUPAR','BOGOTA D.C','NEIVA','RIOHACHA','PASTO','CÚCUTA','ARMENIA','SAN ANDRÉS']
+    spark = SparkSession.builder.appName('UnirCiudadesConID').getOrCreate()
+    output_path='hdfs:///user/hadoop/data_project/features/full_data'
+    dfs=[]
+    for i,city in enumerate(CITIES):
+        path=f'hdfs:///user/hadoop/data_project/enriched/{city}'
+        try:
+            df_city = spark.read.parquet(path)
+            df_city = city_column(df_city,i)
+            dfs.append(df_city)
+        except AnalysisException as e:
+            print(f"Error processing {city}: {e}")
+    df_join=dfs[0]
+    for df in dfs[1:]:
+        df_join=df_join.unionByName(df)
+    print("Datos unidos:")
+    print(df_join.show(5))
+    df_join.printSchema()
+    df_join.write.mode('overwrite').parquet(output_path)
     
